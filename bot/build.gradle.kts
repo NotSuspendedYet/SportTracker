2f7aac23d68fab51093ce070e2a98e9c9eb89ab3
NotSuspendedYet <usefulbk@bk.ru>
2025-06-26 08:45:40 +0300

    10

--- bot/build.gradle.kts
+++ bot/build.gradle.kts
@@ -7,8 +7,16 @@ dependencies {
     implementation(project(":domain"))
     implementation(project(":data"))
     implementation("io.ktor:ktor-server-core-jvm:2.3.10")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.10")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.10")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.10")
-    implementation("io.github.kotlin-telegram-bot:kotlin-telegram-bot:6.1.0")
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.1.0")
     implementation("ch.qos.logback:logback-classic:1.4.14")
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+tasks.withType<Copy> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 } 
