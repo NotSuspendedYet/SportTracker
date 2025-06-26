709c7eaa2cf341268ab38d03df33a6a899b58455
NotSuspendedYet <usefulbk@bk.ru>
2025-06-26 07:01:39 +0300

    2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,4 @@ allprojects {
     repositories {
         mavenCentral()
     }
-}
-
-subprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-
-    dependencies {
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    }
 } 
